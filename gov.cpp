d3b738700b221abf9137c9b4fe91d99ee44920fc
Shoaib A. Khan <shoaib.amjad@gmail.com>
2020-10-17 01:57:55 -0400

    Updates

--- gov.cpp
+++ gov.cpp
@@ -1,20 +1,11 @@
 #include "gov.h"
 
-
 Gov::Gov()
 {
-
 }
 
 Gov::~Gov()
 {
-
-}
-
-void Gov::DeriveRiskList(std::vector<Triple> List)
-{
-    
-
 }
 
 void Gov::InitKnownInfected(std::vector<unsigned int> infected)
@@ -29,13 +20,41 @@ void Gov::ComputeSetKhi()
         uint8_t hashOut[crypto_core_ed25519_BYTES];
         uint8_t hashIn[sizeof(unsigned int)];
         unsigned int X = knownInfected[i];
-
         memcpy(&hashIn, &X, sizeof(X));
         crypto_hash_sha256(hashOut, hashIn, sizeof(unsigned int));
         crypto_core_ed25519_scalar_reduce(hashOut, hashOut); //compute H(X_i)
         std::vector<unsigned char> vecHx(hashOut, hashOut + crypto_core_ed25519_BYTES);
-        vectorKhi.insert(vecHx);
+        vectorKhi.push_back(vecHx);
     }
+}
+
+void Gov::DeriveRiskList(std::vector<Triple> storeList)
+{
+    for (int i = 0; i < storeList.size(); i++)
+    {
+        Triple T = storeList[i];
+        std::vector<std::vector<unsigned char>>::iterator itr;
+        itr = find(vectorKhi.begin(), vectorKhi.end(), T.first);
+        if (itr != vectorKhi.end())
+        {
+            Twople R;
+            unsigned char s[crypto_core_ed25519_BYTES];
+            crypto_core_ed25519_scalar_random(s); //sample random s
+            R.first = Compute_TS(T.second, s);
+            R.second = Compute_TS(T.third, s);
+            RiskList.push_back(R);
+        }
+    }
+}
+
+std::vector<unsigned char> Gov::Compute_TS(std::vector<unsigned char> T, unsigned char s[])
+{
+    unsigned char Tar[crypto_core_ed25519_BYTES];
+    unsigned char Ts[crypto_core_ed25519_BYTES];
+    std::copy(T.begin(), T.end(), Tar);                                 //vector to array
+    crypto_scalarmult_ed25519(Ts, s, Tar);                               //  Ts = Tar^s
+    std::vector<unsigned char> vecTs(Ts, Ts + crypto_core_ed25519_BYTES); //array to vector
+    return vecTs;
 }
 
 bool Gov::UpdateBoard()
--- gov.h
+++ gov.h
@@ -5,7 +5,7 @@
 #include "triple.h"
 #include <vector>
 #include <cstring>
-#include <unordered_set>
+#include <algorithm>
 
 class Gov
 {
@@ -13,14 +13,17 @@ class Gov
 private:
     
     std::vector<unsigned int> knownInfected;    //id's of known infected persons {X_1 ... X_k}
-    std::unordered_set<std::vector<unsigned char>> vectorKhi; 
+    std::vector<std::vector<unsigned char>> vectorKhi; 
+    std::vector<Twople> RiskList;
+    
 
 public:
     Gov();
     ~Gov();
     void InitKnownInfected(std::vector<unsigned int> infected);
     void ComputeSetKhi();
-    void DeriveRiskList(std::vector<Triple> List);
+    void DeriveRiskList(std::vector<Triple> storeList);
+    std::vector<unsigned char> Compute_TS(std::vector<unsigned char> T, unsigned char s[] );
     bool UpdateBoard();
 
 };
